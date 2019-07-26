0e4669240de3c8f2d3f1176276d64af50a555b2a
Fatih Cinar <fatihcinar96@gmail.com>
2019-07-26 16:31:05 +0300

    not a number problem when the price deleted is solved.

--- main.js
+++ main.js
@@ -8,7 +8,17 @@ $("#selectTo").change(function () {
         var base = $.get("https://api.exchangeratesapi.io/latest?base=" + selectBase.options[selectBase.selectedIndex].value, function (data) {
         
             var total = parseFloat(data.rates[selectTo.options[selectTo.selectedIndex].value]) * parseFloat($("#price").val());
-            $("#currency").text(total.toString() + " " + selectTo.options[selectTo.selectedIndex].text);
+
+            if(isNaN(total)){
+                $("#currency").text("Please enter a price");
+                
+                console.log(total)
+            }
+            else{
+                $("#currency").text(total.toString() + " " + selectTo.options[selectTo.selectedIndex].text);
+
+            }
+            
         })
     },1000);
    
